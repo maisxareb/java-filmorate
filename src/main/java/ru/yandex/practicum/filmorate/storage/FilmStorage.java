47d51e12b7c794ad058caaf03a8d7ffe9d1d47bc
Арутюнян Маис <maisxarb@mail.ru>
2025-06-13 05:25:22 +0500

    Реализованы REST API для пользователей и фильмов, добавлена обработка ошибок, внедрение зависимостей, переработана архитектура хранения.

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,10 +6,16 @@ import java.util.Collection;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film add(Film film);
+
     Film update(Film film);
+
     void delete(Integer id);
+
     Film getById(Integer id);
+
     Collection<Film> getAll();
+
     List<Film> getPopular(int count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
 
 public interface UserStorage {
+
     User add(User user);
+
     User update(User user);
+
     void delete(Integer id);
+
     User getById(Integer id);
+
     Collection<User> getAll();
 }
